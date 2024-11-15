55c256f92eeab7b32aa860038d7f22a7e4ae7119
Dominic Go <dominic@dominicgo.dev>
2024-11-16 00:03:49 +0800

    ⭐️ Impl: `CGPoint.getAngleAlongCircle`

--- Sources/Extension+Helpers/CGPoint+Helpers.swift
+++ Sources/Extension+Helpers/CGPoint+Helpers.swift
@@ -31,4 +31,14 @@ public extension CGPoint {
     
     return sqrt(deltaX * deltaX + deltaY * deltaY);
   };
+  
+  /// calculate angle of point with respect to the center
+  func getAngleAlongCircle(withCenter center: Self) -> Angle<CGFloat> {
+    let deltaX = self.x - center.x;
+    let deltaY = self.y - center.y;
+    
+    let angleInRadians = atan2(deltaY, deltaX);
+    return .radians(angleInRadians).normalized;
+  };
+  
 };
