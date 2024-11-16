135bb5484992896b274ac27e622a5b5fcc2a58c3
Dominic Go <dominic@dominicgo.dev>
2024-11-17 01:47:44 +0800

    ❌ Remove: `Array.scalePointsToFitRectangle`

--- Sources/Extension+Helpers/Array+CGPoint.swift
+++ Sources/Extension+Helpers/Array+CGPoint.swift
@@ -27,32 +27,7 @@ public extension Array where Element == CGPoint {
       maxY: maxY
     );
   };
-  
-  func scalePointsToFitRectangle(targetRect: CGRect) -> [CGPoint] {
-    // Find the bounding box for the points
-    let minX = self.min { $0.x < $1.x }?.x ?? 0
-    let maxX = self.max { $0.x < $1.x }?.x ?? 0
-    let minY = self.min { $0.y < $1.y }?.y ?? 0
-    let maxY = self.max { $0.y < $1.y }?.y ?? 0
-    
-    // Calculate the scaling factors
-    let scaleX = targetRect.width / (maxX - minX)
-    let scaleY = targetRect.height / (maxY - minY)
-    
-    // Determine the scaling factor to keep the aspect ratio
-    let scaleFactor = Swift.min(scaleX, scaleY)
-    
-    // Create the scaled points
-    var scaledPoints: [CGPoint] = []
-    for point in self {
-        let scaledX = (point.x - minX) * scaleFactor + targetRect.origin.x
-        let scaledY = (point.y - minY) * scaleFactor + targetRect.origin.y
-        scaledPoints.append(CGPoint(x: scaledX, y: scaledY))
-    }
-    
-    return scaledPoints
-  }
-  
+
   func scalePointsToFit(
     targetRect: CGRect,
     shouldPreserveAspectRatio: Bool = false
