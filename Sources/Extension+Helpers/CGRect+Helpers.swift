5d4b8b129f10e9ebea70d97e46eac2095648fbf2
Dominic Go <dominic@dominicgo.dev>
2024-11-17 02:23:54 +0800

    💄 Gloss: Format Code

--- Sources/Extension+Helpers/CGRect+Helpers.swift
+++ Sources/Extension+Helpers/CGRect+Helpers.swift
@@ -52,12 +52,18 @@ public extension CGRect {
     };
     
     let newX: CGFloat = {
-      guard let midX = midX else { return self.minX };
+      guard let midX = midX else {
+        return self.minX;
+      };
+      
       return midX - (self.width / 2);
     }();
     
     let newY: CGFloat = {
-      guard let midY = midY else { return self.minY };
+      guard let midY = midY else {
+        return self.minY;
+      };
+      
       return midY - (self.height / 2);
     }();
     
@@ -73,16 +79,35 @@ public extension CGRect {
     };
     
     let newX: CGFloat = {
-      guard let maxX = maxX else { return self.minX };
+      guard let maxX = maxX else {
+        return self.minX;
+      };
+      
       return maxX - self.width;
     }();
     
     let newY: CGFloat = {
-      guard let maxY = maxY else { return self.minY };
+      guard let maxY = maxY else {
+        return self.minY;
+      };
+      
       return maxY - self.height;
     }();
     
     self.origin = CGPoint(x: newX, y: newY);
+  };
+  
+  
+  func scale(toNewSize newSize: CGSize) -> Self {
+    let center = self.centerPoint;
+
+    let newX = center.x - (newSize.width / 2);
+    let newY = center.y - (newSize.height / 2);
+    
+    return .init(
+      origin: .init(x: newX, y: newY),
+      size: newSize
+    );
   };
   
   func scale(
@@ -101,17 +126,5 @@ public extension CGRect {
   
   func scale(byScaleFactor scaleFactor: CGFloat) -> Self {
     return self.scale(widthBy: scaleFactor, heightBy: scaleFactor);
-  };
-  
-  func scale(toNewSize newSize: CGSize) -> Self {
-    let center = self.centerPoint;
-
-    let newX = center.x - (newSize.width / 2);
-    let newY = center.y - (newSize.height / 2);
-    
-    return .init(
-      origin: .init(x: newX, y: newY),
-      size: newSize
-    );
   };
 };
