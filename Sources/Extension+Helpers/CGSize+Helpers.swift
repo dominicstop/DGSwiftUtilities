943f1679a5452fde131f000e0f29126b3774caac
Dominic Go <dominic@dominicgo.dev>
2024-11-22 04:26:17 +0800

    ⭐️ Impl: `CGSize+Helpers`

--- Sources/Extension+Helpers/CGSize+Helpers.swift
+++ Sources/Extension+Helpers/CGSize+Helpers.swift
@@ -8,7 +8,42 @@
 import UIKit
 
 public extension CGSize {
+
   var isZero: Bool {
     self == .zero || (self.width == 0 && self.height == 0);
+  };
+  
+  func getScaleFactor(
+    scalingTo newScale: Self,
+    shouldPreserveAspectRatio: Bool
+  ) -> (
+    scaleX: CGFloat,
+    scaleY: CGFloat
+  ) {
+    let scaleFactorWidth = newScale.width / newScale.width;
+    let scaleFactorHeight = newScale.height / newScale.height;
+    
+    guard shouldPreserveAspectRatio  else {
+      return (scaleFactorWidth, scaleFactorHeight);
+    };
+    
+    let minScaleFactor = min(scaleFactorWidth, scaleFactorHeight);
+    return (minScaleFactor, minScaleFactor);
+  };
+  
+  func createScaleTransform(
+    scalingTo newScale: Self,
+    shouldPreserveAspectRatio: Bool
+  ) -> CGAffineTransform {
+  
+    let scaleFactor = self.getScaleFactor(
+      scalingTo: newScale,
+      shouldPreserveAspectRatio: shouldPreserveAspectRatio
+    );
+    
+    return .init(
+      scaleX: scaleFactor.scaleX,
+      y: scaleFactor.scaleY
+    );
   };
 };
