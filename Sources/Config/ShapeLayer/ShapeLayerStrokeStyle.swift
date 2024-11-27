f44b2a30964d1e75783683dd0750ecd0d3ab792a
Dominic Go <dominic@dominicgo.dev>
2024-11-27 10:01:11 +0800

    ⭐️ Impl: `ShapeLayerStrokeStyle.createAnimations`

--- Sources/Config/ShapeLayer/ShapeLayerStrokeStyle.swift
+++ Sources/Config/ShapeLayer/ShapeLayerStrokeStyle.swift
@@ -45,6 +45,100 @@ public struct ShapeLayerStrokeStyle {
     shapeLayer.strokeStart = self.strokeStart;
     shapeLayer.strokeEnd = self.strokeEnd;
   };
+  
+  public func createAnimations(
+    forShape shapeLayer: CAShapeLayer,
+    withPrevStyle prevStyle: Self,
+    usingBaseAnimation baseAnimation: CABasicAnimation
+  ) -> [CABasicAnimation] {
+  
+    Self.stylePropertyToLayerKeyPathMap.reduce(into: []) {
+      guard shapeLayer.animation(forKey: $1.layerKeyPath) == nil else {
+        return;
+      };
+      
+      switch $1.styleKeyPath {
+        case let styleKeyPath as KeyPath<Self, CGFloat>:
+          let prevValue = prevStyle[keyPath: styleKeyPath];
+          let nextValue = self[keyPath: styleKeyPath];
+          
+          guard nextValue != prevValue else {
+            return;
+          };
+          
+          let animation = baseAnimation.copy() as! CABasicAnimation;
+          $0.append(animation);
+          
+          animation.keyPath = $1.layerKeyPath;
+          animation.fromValue = prevValue;
+          animation.toValue = nextValue;
+          
+          shapeLayer.setValue(
+            nextValue,
+            forKeyPath: $1.layerKeyPath
+          );
+          
+          shapeLayer.add(animation, forKey: $1.layerKeyPath);
+      
+        case let styleKeyPath as KeyPath<Self, UIColor?>:
+          let prevValue = prevStyle[keyPath: styleKeyPath];
+          let nextValue = self[keyPath: styleKeyPath];
+          
+          guard nextValue?.components != prevValue?.components else {
+            return;
+          };
+          
+          let animation = baseAnimation.copy() as! CABasicAnimation;
+          $0.append(animation);
+          
+          animation.keyPath = $1.layerKeyPath;
+          animation.fromValue = prevValue?.cgColor;
+          animation.toValue = nextValue?.cgColor;
+          
+          shapeLayer.setValue(
+            nextValue?.cgColor,
+            forKeyPath: $1.layerKeyPath
+          );
+          
+          shapeLayer.add(animation, forKey: $1.layerKeyPath);
+          
+        case let styleKeyPath as KeyPath<Self, LineDashPattern>:
+          let prevValue = prevStyle[keyPath: styleKeyPath];
+          let nextValue = self[keyPath: styleKeyPath];
+          
+          guard nextValue.values != prevValue.values else {
+            return;
+          };
+          
+          let animation = baseAnimation.copy() as! CABasicAnimation;
+          $0.append(animation);
+          
+          animation.keyPath = $1.layerKeyPath;
+          animation.fromValue = prevValue.valuesObjc;
+          animation.toValue = nextValue.valuesObjc;
+          
+          shapeLayer.setValue(
+            nextValue.valuesObjc,
+            forKeyPath: $1.layerKeyPath
+          );
+            
+          shapeLayer.add(
+            animation,
+            forKey: $1.layerKeyPath
+          );
+          
+        default:
+          #if DEBUG
+          assertionFailure(
+            "Unimplemented animation key"
+            + "\n - for styleKeyPath: \($1.styleKeyPath)"
+            + "\n - for layerKeyPath: \($1.layerKeyPath)"
+          );
+          #endif
+          break;
+      };
+    };
+  };
 };
 
 // MARK: - ShapeLayerStrokeStyle+StaticAlias
@@ -58,8 +152,8 @@ public extension ShapeLayerStrokeStyle {
       strokeColor: nil,
       linePattern: .noPattern,
       lineDashPhase: 0,
-      strokeStart: 1,
-      strokeEnd: 0
+      strokeStart: 0,
+      strokeEnd: 1
     );
   };
   
@@ -69,5 +163,43 @@ public extension ShapeLayerStrokeStyle {
       strokeColor: nil,
       linePattern: .noPattern
     );
+  };
+};
+
+// MARK: - ShapeLayerStrokeStyle+StaticMembers
+// -------------------------------------------
+
+public extension ShapeLayerStrokeStyle {
+  
+  static var stylePropertyToLayerKeyPathMap: [(
+    styleKeyPath: PartialKeyPath<Self>,
+    layerKeyPath: String
+  )] {
+    [
+      (
+        \.lineWidth,
+        #keyPath(CAShapeLayer.lineWidth)
+      ),
+      (
+        \.strokeColor,
+        #keyPath(CAShapeLayer.strokeColor)
+      ),
+      (
+        \.linePattern,
+        #keyPath(CAShapeLayer.lineDashPattern)
+      ),
+      (
+        \.lineDashPhase,
+        #keyPath(CAShapeLayer.lineDashPhase)
+      ),
+      (
+        \.strokeStart,
+        #keyPath(CAShapeLayer.strokeStart)
+      ),
+      (
+        \.strokeEnd,
+        #keyPath(CAShapeLayer.strokeEnd)
+      )
+    ];
   };
 };
