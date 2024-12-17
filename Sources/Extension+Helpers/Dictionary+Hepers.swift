fac50d16ef2c208ebf74fe3bb54fb621f3697fda
Dominic Go <dominic@dominicgo.dev>
2024-12-17 13:08:50 +0800

    💄 Gloss: Re-Arrange Code

--- Sources/Extension+Helpers/Dictionary+Hepers.swift
+++ Sources/Extension+Helpers/Dictionary+Hepers.swift
@@ -113,6 +113,79 @@ public extension Dictionary where Key == String {
     };
   };
   
+  func getValue<T>(
+    forKey key: String,
+    type: T.Type = T.self,
+    fallbackValue: T
+  ) -> T {
+  
+    let value = try? self.getValue(
+      forKey: key,
+      type: type
+    );
+    
+    return  value ?? fallbackValue;
+  };
+  
+  func getValue<T: RawRepresentable, U>(
+    forKey key: String,
+    type: T.Type = T.self,
+    rawValueType: U.Type = T.RawValue.self
+  ) throws -> T where T: RawRepresentable<U> {
+  
+    let rawValue = try? self.getValue(
+      forKey: key,
+      type: U.self
+    );
+    
+    guard let rawValue = rawValue else {
+      throw GenericError(
+        errorCode: .typeCastFailed,
+        description: "Unable to cast value to RawRepresentable.RawValue type",
+        extraDebugValues: [
+          "key": key,
+          "type": type.self,
+          "rawValueType": U.self,
+        ]
+      );
+    };
+    
+    let value = T.init(rawValue: rawValue);
+    guard let value = value else {
+      throw GenericError(
+        errorCode: .invalidValue,
+        description: "No matching value in enum",
+        extraDebugValues: [
+          "key": key,
+          "type": type.self,
+          "rawValueType": U.self,
+        ]
+      );
+    };
+    
+    return value;
+  };
+  
+  func getValue<T: RawRepresentable, U>(
+    forKey key: String,
+    type: T.Type = T.self,
+    rawValueType: U.Type = T.RawValue.self,
+    fallbackValue: T
+  ) -> T where T: RawRepresentable<U> {
+  
+    let enumValue = try? self.getValue(
+      forKey: key,
+      type: T.self,
+      rawValueType: U.self
+    );
+    
+    guard let enumValue = enumValue else {
+      return fallbackValue;
+    };
+    
+    return enumValue;
+  };
+  
   func getColor(forKey key: String) throws -> UIColor {
     guard let colorValue = self[key] else {
       throw GenericError(
@@ -204,79 +277,6 @@ public extension Dictionary where Key == String {
       forKey: dictValue,
       valueType: KeyPathValue.self
     );
-  };
-  
-  func getValue<T>(
-    forKey key: String,
-    type: T.Type = T.self,
-    fallbackValue: T
-  ) -> T {
-  
-    let value = try? self.getValue(
-      forKey: key,
-      type: type
-    );
-    
-    return  value ?? fallbackValue;
-  };
-  
-  func getValue<T: RawRepresentable, U>(
-    forKey key: String,
-    type: T.Type = T.self,
-    rawValueType: U.Type = T.RawValue.self
-  ) throws -> T where T: RawRepresentable<U> {
-  
-    let rawValue = try? self.getValue(
-      forKey: key,
-      type: U.self
-    );
-    
-    guard let rawValue = rawValue else {
-      throw GenericError(
-        errorCode: .typeCastFailed,
-        description: "Unable to cast value to RawRepresentable.RawValue type",
-        extraDebugValues: [
-          "key": key,
-          "type": type.self,
-          "rawValueType": U.self,
-        ]
-      );
-    };
-    
-    let value = T.init(rawValue: rawValue);
-    guard let value = value else {
-      throw GenericError(
-        errorCode: .invalidValue,
-        description: "No matching value in enum",
-        extraDebugValues: [
-          "key": key,
-          "type": type.self,
-          "rawValueType": U.self,
-        ]
-      );
-    };
-    
-    return value;
-  };
-  
-  func getValue<T: RawRepresentable, U>(
-    forKey key: String,
-    type: T.Type = T.self,
-    rawValueType: U.Type = T.RawValue.self,
-    fallbackValue: T
-  ) -> T where T: RawRepresentable<U> {
-  
-    let enumValue = try? self.getValue(
-      forKey: key,
-      type: T.self,
-      rawValueType: U.self
-    );
-    
-    guard let enumValue = enumValue else {
-      return fallbackValue;
-    };
-    
-    return enumValue;
   };
   
   func getArray<T>(
