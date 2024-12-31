07c598eb0eb44a3863fe987e3e40ebf21515dacf
Dominic Go <dominic@dominicgo.dev>
2025-01-01 03:47:14 +0800

    ⭐️ Impl: `UIMenuElementState+RawValueToStringConvertible`

--- Sources/Extension+RawValueToStringConvertible/UIMenuElementState+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/UIMenuElementState+RawValueToStringConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  UIMenuElementState+EnumCaseStringRepresentable.swift
+//  UIMenuElementState+RawValueToStringConvertible.swift
 //
 //
 //  Created by Dominic Go on 12/18/23.
@@ -8,24 +8,36 @@
 import UIKit
 
 @available(iOS 13.0, *)
-extension UIMenuElement.State: EnumCaseStringRepresentable {
-  
+extension UIMenuElement.State: RawValueToStringConvertible {
+
+  // MARK: - CaseIterable
+  // --------------------
+
+  public static var allCases: [Self] = [
+    .off,
+    .on,
+    .mixed,
+  ];
+
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
+
   public var caseString: String {
     switch self {
       case .off:
         return "off";
-        
+
       case .on:
         return "on";
-        
+
       case .mixed:
         return "mixed";
-      
+
       @unknown default:
         #if DEBUG
         print("Runtime Warning - Not implemented -", #file);
         #endif
-        
+
         return "";
     };
   };
