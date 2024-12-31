a04271efb0f24ce550cc7029af518bdd3f70a4e4
Dominic Go <dominic@dominicgo.dev>
2025-01-01 04:00:03 +0800

    ⭐️ Impl: `UIInterfaceOrientation+RawValueToStringConvertible`

--- Sources/Extension+RawValueToStringConvertible/UIInterfaceOrientation+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/UIInterfaceOrientation+RawValueToStringConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  UIInterfaceOrientation+EnumCaseStringRepresentable.swift
+//  UIInterfaceOrientation+RawValueToStringConvertible.swift
 //
 //
 //  Created by Dominic Go on 12/18/23.
@@ -7,29 +7,44 @@
 
 import UIKit
 
-extension UIInterfaceOrientation: EnumCaseStringRepresentable {
+extension UIInterfaceOrientation: RawValueToStringConvertible {
+
+  // MARK: - CaseIterable
+  // --------------------
+
+  public static var allCases: [Self] = [
+    .unknown,
+    .portrait,
+    .portraitUpsideDown,
+    .landscapeLeft,
+    .landscapeRight,
+  ];
+
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
+
   public var caseString: String {
     switch self {
       case .unknown:
         return "unknown";
-        
+
       case .portrait:
         return "portrait";
-        
+
       case .portraitUpsideDown:
         return "portraitUpsideDown";
-        
+
       case .landscapeLeft:
         return "landscapeLeft";
-        
+
       case .landscapeRight:
         return "landscapeRight";
-        
+
       @unknown default:
         #if DEBUG
         print("Runtime Warning - Not implemented -", #file);
         #endif
-        
+
         return "";
     };
   };
