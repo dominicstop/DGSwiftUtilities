1177d52f6365e7c80d16f84886b98604df869e8b
Dominic Go <dominic@dominicgo.dev>
2025-01-01 03:32:22 +0800

    ⭐️ Impl: `UIVibrancyEffectStyle+RawValueToStringConvertible`

--- Sources/Extension+RawValueToStringConvertible/UIVibrancyEffectStyle+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/UIVibrancyEffectStyle+RawValueToStringConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  UIVibrancyEffectStyle+EnumCaseStringRepresentable.swift
+//  UIVibrancyEffectStyle+RawValueToStringConvertible.swift
 //
 //
 //  Created by Dominic Go on 6/22/24.
@@ -8,34 +8,50 @@
 import UIKit
 
 @available(iOS 13.0, *)
-extension UIVibrancyEffectStyle: EnumCaseStringRepresentable  {
+extension UIVibrancyEffectStyle: RawValueToStringConvertible {
+
+  public static var allCases: [UIVibrancyEffectStyle] {
+    return [
+      .label,
+      .secondaryLabel,
+      .tertiaryLabel,
+      .quaternaryLabel,
+      .fill,
+      .secondaryFill,
+      .tertiaryFill,
+      .separator,
+    ];
+  };
+
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
 
   public var caseString: String {
     switch self {
       case .label:
         return "label";
-        
+
       case .secondaryLabel:
         return "secondaryLabel";
-        
+
       case .tertiaryLabel:
         return "tertiaryLabel";
-        
+
       case .quaternaryLabel:
         return "quaternaryLabel";
-        
+
       case .fill:
         return "fill";
-        
+
       case .secondaryFill:
         return "secondaryFill";
-        
+
       case .tertiaryFill:
         return "tertiaryFill";
-        
+
       case .separator:
         return "separator";
-        
+
       @unknown default:
         return "default";
     };
