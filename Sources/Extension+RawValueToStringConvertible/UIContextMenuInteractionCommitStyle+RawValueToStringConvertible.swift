daa405c9df552a0227067a8c68bcedaddcb424a9
Dominic Go <dominic@dominicgo.dev>
2025-01-01 03:17:33 +0800

    ⭐️ Impl: `UIContextMenuInteractionCommitStyle+RawValueToStringConvertible`

--- Sources/Extension+RawValueToStringConvertible/UIContextMenuInteractionCommitStyle+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/UIContextMenuInteractionCommitStyle+RawValueToStringConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  UIContextMenuInteractionCommitStyle+EnumCaseStringRepresentable.swift
+//  UIContextMenuInteractionCommitStyle+RawValueToStringConvertible.swift
 //
 //
 //  Created by Dominic Go on 12/18/23.
@@ -8,21 +8,32 @@
 import UIKit
 
 @available(iOS 13.0, *)
-extension UIContextMenuInteractionCommitStyle: EnumCaseStringRepresentable {
+extension UIContextMenuInteractionCommitStyle: RawValueToStringConvertible {
+  
+  // MARK: - CaseIterable
+  // --------------------
+  
+  public static var allCases: [Self] = [
+    .dismiss,
+    .pop,
+  ];
+
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
 
   public var caseString: String {
     switch self {
       case .dismiss:
         return "dismiss";
-        
+
       case .pop:
         return "pop";
-      
+
       @unknown default:
         #if DEBUG
         print("Runtime Warning - Not implemented -", #file);
         #endif
-        
+
         return "";
     };
   };
