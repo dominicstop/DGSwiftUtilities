29fc8dec4d8ad08e7bbcbaedeeb45caf3696e237
Dominic Go <dominic@dominicgo.dev>
2025-01-01 03:40:36 +0800

    ⭐️ Impl: `UIUserInterfaceIdiom+RawValueToStringConvertible`

--- Sources/Extension+RawValueToStringConvertible/UIUserInterfaceIdiom+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/UIUserInterfaceIdiom+RawValueToStringConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  UIUserInterfaceIdiom+EnumCaseStringRepresentable.swift
+//  UIUserInterfaceIdiom+RawValueToStringConvertible.swift
 //  
 //
 //  Created by Dominic Go on 12/17/23.
@@ -8,7 +8,36 @@
 import UIKit
 
 
-extension UIUserInterfaceIdiom: EnumCaseStringRepresentable, CustomStringConvertible {
+extension UIUserInterfaceIdiom: RawValueToStringConvertible {
+
+  // MARK: - CaseIterable
+  // --------------------
+
+  public static var allCases: [Self] {
+    var cases: [Self] = [
+      .unspecified,
+    ];
+    
+    if #available(iOS 3.2, *) {
+      cases.append(.phone);
+      cases.append(.pad);
+    };
+
+    
+    if #available(iOS 9.0, *) {
+      cases.append(.tv);
+      cases.append(.carPlay);
+    };
+    
+    if #available(iOS 14.0, *) {
+      cases.append(.mac);
+    };
+    
+    return cases;
+  };
+
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
 
   public var caseString: String {
     switch self {
@@ -29,19 +58,19 @@ extension UIUserInterfaceIdiom: EnumCaseStringRepresentable, CustomStringConvert
 
       case .mac:
         return "mac";
-      
+
       #if !targetEnvironment(macCatalyst)
       #if swift(>=5.9)
       case .vision:
         return "vision";
       #endif
       #endif
-      
+
       @unknown default:
         #if DEBUG
         print("Runtime Warning - Not implemented -", #file);
         #endif
-        
+
         return "";
     };
   };
