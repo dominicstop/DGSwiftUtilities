6784c13d9d9582f11cfe167a62abe909014f8d3c
Dominic Go <dominic@dominicgo.dev>
2025-01-01 03:42:17 +0800

    ⭐️ Impl: `UIUserInterfaceActiveAppearance+RawValueToStringConvertible`

--- Sources/Extension+RawValueToStringConvertible/UIUserInterfaceActiveAppearance+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/UIUserInterfaceActiveAppearance+RawValueToStringConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  UIUserInterfaceActiveAppearance+EnumCaseStringRepresentable.swift
+//  UIUserInterfaceActiveAppearance+RawValueToStringConvertible.swift
 //
 //
 //  Created by Dominic Go on 12/18/23.
@@ -8,26 +8,37 @@
 import UIKit
 
 @available(iOS 14.0, *)
-extension UIUserInterfaceActiveAppearance: EnumCaseStringRepresentable, CustomStringConvertible {
+extension UIUserInterfaceActiveAppearance: RawValueToStringConvertible {
+
+  // MARK: - CaseIterable
+  // --------------------
+
+  public static var allCases: [Self] = [
+    .unspecified,
+    .inactive,
+    .active,
+  ];
+
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
 
   public var caseString: String {
     switch self {
       case .unspecified:
         return "unspecified";
-        
+
       case .inactive:
         return "inactive";
-        
+
       case .active:
         return "active";
-        
+
       @unknown default:
         #if DEBUG
         print("Runtime Warning - Not implemented -", #file);
         #endif
-        
+
         return "";
     };
   };
 };
-
