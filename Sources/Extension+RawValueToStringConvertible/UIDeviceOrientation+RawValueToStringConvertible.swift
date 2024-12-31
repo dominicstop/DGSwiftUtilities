075dfcf2b7d7c596e9e189a417b28869fd457a85
Dominic Go <dominic@dominicgo.dev>
2025-01-01 03:25:17 +0800

    ⭐️ Imp: `UIDeviceOrientation+RawValueToStringConvertible`

--- Sources/Extension+RawValueToStringConvertible/UIDeviceOrientation+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/UIDeviceOrientation+RawValueToStringConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  UIDeviceOrientation+EnumCaseStringRepresentable.swift
+//  UIDeviceOrientation+RawValueToStringConvertible.swift
 //
 //
 //  Created by Dominic Go on 12/17/23.
@@ -7,36 +7,52 @@
 
 import UIKit
 
-extension UIDeviceOrientation: EnumCaseStringRepresentable, CustomStringConvertible {
+extension UIDeviceOrientation: RawValueToStringConvertible {
+
+  // MARK: - CaseIterable
+  // --------------------
+
+  public static let allCases: [Self] = [
+    .unknown,
+    .portrait,
+    .portraitUpsideDown,
+    .landscapeLeft,
+    .landscapeRight,
+    .faceUp,
+    .faceDown,
+  ];
+
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
 
   public var caseString: String {
     switch self {
       case .unknown:
         return "unknown";
-        
+
       case .portrait:
         return "portrait";
-        
+
       case .portraitUpsideDown:
         return "portraitUpsideDown";
-        
+
       case .landscapeLeft:
         return "landscapeLeft";
-        
+
       case .landscapeRight:
         return "landscapeRight";
-        
+
       case .faceUp:
         return "faceUp";
-        
+
       case .faceDown:
         return "faceDown";
-        
+
       @unknown default:
         #if DEBUG
         print("Runtime Warning - Not implemented -", #file);
         #endif
-        
+
         return Self.unknown.caseString;
     }
   };
