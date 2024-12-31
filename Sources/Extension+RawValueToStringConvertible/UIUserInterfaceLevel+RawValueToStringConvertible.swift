df400864a39ae4a51c2c6fdef186a62437a49d86
Dominic Go <dominic@dominicgo.dev>
2025-01-01 03:38:24 +0800

    ⭐️ Impl: `UIUserInterfaceSizeClass+CaseIterable`

--- Sources/Extension+RawValueToStringConvertible/UIUserInterfaceLevel+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/UIUserInterfaceLevel+RawValueToStringConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  UIUserInterfaceLevel+EnumCaseStringRepresentable.swift
+//  UIUserInterfaceLevel+RawValueToStringConvertible.swift
 //
 //
 //  Created by Dominic Go on 12/17/23.
@@ -8,24 +8,36 @@
 import UIKit
 
 @available(iOS 13.0, *)
-extension UIUserInterfaceLevel: EnumCaseStringRepresentable, CustomStringConvertible {
+extension UIUserInterfaceLevel: RawValueToStringConvertible {
+
+  // MARK: - CaseIterable
+  // --------------------
+
+  public static var allCases: [Self] = [
+    .unspecified,
+    .base,
+    .elevated,
+  ];
+
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
 
   public var caseString: String {
     switch self {
       case .unspecified:
         return "unspecified";
-        
+
       case .base:
         return "base";
-        
+
       case .elevated:
         return "elevated";
-        
+
       @unknown default:
         #if DEBUG
         print("Runtime Warning - Not implemented -", #file);
         #endif
-        
+
         return "";
     };
   };
