48cb69262116afdfc1755ce41889947c36d5fa66
Dominic Go <dominic@dominicgo.dev>
2025-01-01 03:44:28 +0800

    ⭐️ Impl: `UITraitEnvironmentLayoutDirection+RawValueToStringConvertible`

--- Sources/Extension+RawValueToStringConvertible/UITraitEnvironmentLayoutDirection+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/UITraitEnvironmentLayoutDirection+RawValueToStringConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  UITraitEnvironmentLayoutDirection+EnumCaseStringRepresentable.swift
+//  UITraitEnvironmentLayoutDirection+RawValueToStringConvertible.swift
 //
 //
 //  Created by Dominic Go on 12/18/23.
@@ -7,24 +7,36 @@
 
 import UIKit
 
-extension UITraitEnvironmentLayoutDirection: EnumCaseStringRepresentable, CustomStringConvertible {
+extension UITraitEnvironmentLayoutDirection: RawValueToStringConvertible {
+
+  // MARK: - CaseIterable
+  // --------------------
+
+  public static var allCases: [Self] = [
+    .unspecified,
+    .leftToRight,
+    .rightToLeft,
+  ];
+
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
 
   public var caseString: String {
     switch self {
       case .unspecified:
         return "unspecified";
-        
+
       case .leftToRight:
         return "leftToRight";
-        
+
       case .rightToLeft:
         return "rightToLeft";
-        
+
       @unknown default:
         #if DEBUG
         print("Runtime Warning - Not implemented -", #file);
         #endif
-        
+
         return "";
     };
   };
