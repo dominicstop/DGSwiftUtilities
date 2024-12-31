8a64065fda6da4d6ff7d6921e7105c12972526cf
Dominic Go <dominic@dominicgo.dev>
2025-01-01 03:35:47 +0800

    ⭐️ Impl: `UIUserInterfaceSizeClass+RawValueToStringConvertible`

--- Sources/Extension+RawValueToStringConvertible/UIUserInterfaceSizeClass+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/UIUserInterfaceSizeClass+RawValueToStringConvertible.swift
@@ -1,5 +1,5 @@
 //
-//  UIUserInterfaceSizeClass+EnumCaseStringRepresentable.swift
+//  UIUserInterfaceSizeClass+RawValueToStringConvertible.swift
 //
 //
 //  Created by Dominic Go on 12/17/23.
@@ -7,24 +7,36 @@
 
 import UIKit
 
-extension UIUserInterfaceSizeClass: EnumCaseStringRepresentable, CustomStringConvertible {
+extension UIUserInterfaceSizeClass: RawValueToStringConvertible {
+
+  // MARK: - CaseIterable
+  // --------------------
+
+  public static var allCases: [Self] = [
+    .unspecified,
+    .compact,
+    .regular,
+  ];
+
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
 
   public var caseString: String {
     switch self {
       case .unspecified:
         return "unspecified";
-        
+
       case .compact:
         return "compact";
-        
+
       case .regular:
         return "regular";
-        
+
       @unknown default:
         #if DEBUG
         print("Runtime Warning - Not implemented -", #file);
         #endif
-        
+
         return "";
     };
   };
