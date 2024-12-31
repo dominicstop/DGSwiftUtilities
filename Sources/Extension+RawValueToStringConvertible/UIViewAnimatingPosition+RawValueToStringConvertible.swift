9b025e96f01f4addf3870a50106156a8c537af19
Dominic Go <dominic@dominicgo.dev>
2025-01-01 02:50:14 +0800

    ⭐️ Impl: `UIViewAnimatingPosition+RawValueToStringConvertible`

---
+++ Sources/Extension+RawValueToStringConvertible/UIViewAnimatingPosition+RawValueToStringConvertible.swift
@@ -0,0 +1,44 @@
+//
+//  UIViewAnimatingPosition+RawValueToStringConvertible.swift
+//  
+//
+//  Created by Dominic Go on 1/1/25.
+//
+
+import UIKit
+
+extension UIViewAnimatingPosition: RawValueToStringConvertible {
+  
+  // MARK: - CaseIterable
+  // --------------------
+  
+  public static var allCases: [UIViewAnimatingPosition] = [
+    .start,
+    .current,
+    .end
+  ];
+  
+  // MARK: - StringMappedRawRepresentable
+  // ------------------------------------
+  
+  public var caseString: String {
+    switch self {
+      case .end:
+        return "end";
+        
+      case .start:
+        return "start";
+        
+      case .current:
+        return "current";
+        
+      @unknown default:
+        #if DEBUG
+        fatalError("not impl.");
+        #else
+        return "unknown";
+        #endif
+    };
+  };
+};
+
