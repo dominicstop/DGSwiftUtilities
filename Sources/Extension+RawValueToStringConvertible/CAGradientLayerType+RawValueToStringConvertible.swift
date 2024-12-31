782ea58f713163e71dc3ca1f16963af772fcb733
Dominic Go <dominic@dominicgo.dev>
2025-01-01 04:11:37 +0800

    💫 Update: `CAGradientLayerType+RawValueToStringConvertible`

--- Sources/Extension+RawValueToStringConvertible/CAGradientLayerType+RawValueToStringConvertible.swift
+++ Sources/Extension+RawValueToStringConvertible/CAGradientLayerType+RawValueToStringConvertible.swift
@@ -13,11 +13,19 @@ extension CAGradientLayerType: RawValueToStringConvertible {
   // MARK: - CaseIterable
   // --------------------
   
-  public static var allCases: [Self] = [
-    .axial,
-    .conic,
-    .radial,
-  ];
+  public static var allCases: [Self] = {
+    var items: [Self] = [
+      .axial,
+      .conic,
+      .radial,
+    ];
+    
+    if #available(iOS 12.0, *) {
+      items.append(.conic);
+    };
+    
+    return items;
+  }();
   
   // MARK: - StringMappedRawRepresentable
   // ------------------------------------
