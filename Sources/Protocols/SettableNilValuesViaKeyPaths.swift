6c22472703ff4c8313d79bb2882d865538fc3140
Dominic Go <dominic@dominicgo.dev>
2025-04-13 20:21:53 +0800

    ⭐️ Impl: `SettableNilValuesViaKeyPaths.setNilValuesRecursively `

--- Sources/Protocols/SettableNilValuesViaKeyPaths.swift
+++ Sources/Protocols/SettableNilValuesViaKeyPaths.swift
@@ -16,14 +16,7 @@ public protocol SettableNilValuesViaKeyPaths: SettableNilValues {
 public extension SettableNilValuesViaKeyPaths {
   
   mutating func setNilValues(with otherValue: Self) {
-    Self.allNilValueKeyPaths.forEach {
-      let newValue = otherValue[keyPath: $0.partialKeyPath];
-      
-      try? $0.setValueIfNil(
-        target: &self,
-        withValue: newValue
-      );
-    };
+    self.setNilValuesRecursively(with: otherValue);
   };
 };
 
@@ -44,5 +37,68 @@ public extension SettableNilValuesViaKeyPaths {
       
       return optionalValue.isSome();
     };
+  };
+  
+  mutating func setNilValuesRecursively(with otherValue: Self) {
+    Self.allNilValueKeyPaths.forEach {
+      let newValue = otherValue[keyPath: $0.partialKeyPath];
+      let oldValue = self[keyPath: $0.partialKeyPath];
+      
+      switch (newValue, oldValue) {
+        case (
+          let newValue as Optional<any SettableNilValues>,
+          let oldValue as Optional<any SettableNilValues>
+        ):
+
+          guard let newValue = newValue,
+                oldValue != nil
+          else {
+            fallthrough;
+          };
+          
+          do {
+            // create copy then write to copy
+            var mergedValue = oldValue!;
+            try mergedValue.setNilValues(withSomeValue: newValue);
+            
+            // attempt merge and overwrite
+            try $0.setValue(
+              target: &self,
+              withValue: mergedValue
+            );
+            
+          } catch {
+            fallthrough;
+          };
+          
+        default:
+          let newValue = otherValue[keyPath: $0.partialKeyPath];
+          
+          try? $0.setValueIfNil(
+            target: &self,
+            withValue: newValue
+          );
+      };
+    };
+  };
+};
+
+public extension SettableNilValues {
+  
+  mutating func setNilValues(withSomeValue otherValue: some SettableNilValues) throws {
+    guard let otherValue = otherValue as? Self else {
+      throw GenericError(
+        errorCode: .typeCastFailed,
+        description:
+            "The `otherValue` of type: \(type(of: otherValue)), could not be"
+          + " casted to current type of: \(Self.self).",
+        extraDebugValues: [
+          "self": self,
+          "otherValue": otherValue,
+        ]
+      )
+    };
+    
+    self.setNilValues(with: otherValue);
   };
 };
