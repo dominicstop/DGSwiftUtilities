8feb9ffaa8381f3c2386b26a16b8f7a7a18f5bd6
Dominic Go <dominic@dominicgo.dev>
2025-04-17 01:08:21 +0800

    💫 Update: `KeyframeAppliable.applyBaseKeyframe`

--- Sources/Protocols/Keyframeable/KeyframeAppliable.swift
+++ Sources/Protocols/Keyframeable/KeyframeAppliable.swift
@@ -34,15 +34,41 @@ public extension KeyframeAppliable {
 public extension KeyframeAppliable {
   
   func applyBaseKeyframe(
-    toView targetView: KeyframeTarget
+    toView targetView: KeyframeTarget,
+    withEdgeConstraints edgeConstraint: (
+      constraintLeft: NSLayoutConstraint,
+      constraintRight: NSLayoutConstraint,
+      constraintTop: NSLayoutConstraint,
+      constraintBottom: NSLayoutConstraint
+    )? = nil
   ) where KeyframeTarget: UIView {
   
-    if let baseViewKeyframe = self as? (any BaseViewKeyframeConfig) {
+    if let baseViewKeyframe = self as? (any BaseViewConcreteKeyframe) {
       baseViewKeyframe.applyBaseViewKeyframe(toView: targetView);
     };
     
-    if let baseLayerKeyframe = self as? (any BaseLayerKeyframeConfig) {
-      baseLayerKeyframe.applyBaseLayerKeyframe(toView: targetView);
+    if let baseBorderKeyframe = self as? (any BaseLayerBorderConcreteKeyframe) {
+      baseBorderKeyframe.applyBaseLayerBorderKeyframe(toView: targetView);
+    };
+    
+    if let baseShadowKeyframe = self as? (any BaseLayerShadowConcreteKeyframe) {
+      baseShadowKeyframe.applyBaseLayerShadowKeyframe(toView: targetView);
+    };
+    
+    if let baseCornerRadiusKeyframe = self as? (any BaseLayerSystemCornerRadiusConcreteKeyframe) {
+      baseCornerRadiusKeyframe.applyBaseLayerSystemCornerRadiusKeyframe(toView: targetView);
+    };
+    
+    if let baseFrameLayoutKeyframe = self as? (any BaseFrameLayoutConcreteKeyframe),
+       let edgeConstraint = edgeConstraint
+    {
+      baseFrameLayoutKeyframe.applyBaseLayoutKeyframe(
+        toView: targetView,
+        constraintLeft: edgeConstraint.constraintLeft,
+        constraintRight: edgeConstraint.constraintRight,
+        constraintTop: edgeConstraint.constraintTop,
+        constraintBottom: edgeConstraint.constraintBottom
+      );
     };
   };
 };
